08e4d7b56ac4b334565a48d0221911aed7f04049
dedsec <54960056+BlackstormCoder@users.noreply.github.com>
2023-02-26 15:41:25 +0530

    add custom lsp server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,20 @@
-local lsp = require('lsp-zero')
+-- local lsp = require('lsp-zero')
 
-lsp.preset('recommended')
+-- lsp.preset('recommended')
+-- lsp.setup()
+--
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+-- When you don't have mason.nvim installed
+-- You'll need to list the servers installed in your system
+lsp.setup_servers({'tsserver', 'eslint', 'pyright', 'marksman', 'bash-language-server'})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
 lsp.setup()
